ab0ee9c6c4a53dee933d7ec968a3ff609b13ea00
JGL <luckibarra15@gmail.com>
2025-04-20 14:09:05 -0600

    Agregar dominio de Render a ALLOWED_HOSTS

--- luxury_reservations_bk/settings.py
+++ luxury_reservations_bk/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-k*ic3s-&$3$@y2bs6p&rwz_7*$gjf)0!#l7+n3peuoxe^l%$qb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(',')
 
 
 # Application definition
